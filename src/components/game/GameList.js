aeb56e140007ea293680672585da4723d6b701d2
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-09 15:49:30 -0600

    GameList refactor, gametype specified with label

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -7,7 +7,7 @@ export const GameList = (props) => {
     useEffect(() => {
         getGames()
     }, [])
-
+console.log(games);
     return (
         <article className="games">
             {
@@ -15,7 +15,7 @@ export const GameList = (props) => {
                     return <section key={`game--${game.id}`} className="game">
                         <div className="game__title">{game.title}</div>
                         <div className="game__players">{game.number_of_players} players needed</div>
-                        <div className="game__gametype">Game type is {game.gametype}</div>
+                        <div className="game__gametype">Game type is {game.gametype.label}</div>
                     </section>
                 })
             }
